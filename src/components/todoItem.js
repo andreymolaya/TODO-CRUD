4367da70ce6753f3daf48f0b63eed9e2314c870c
andreymolaya <andreymolaya@gmail.com>
2021-07-26 17:32:17 -0500

    inicio

---
+++ src/components/todoItem.js
@@ -0,0 +1,27 @@
+const TodoItem = ({task, student, id, handleDelete, handlePut, isCompleted}) => {
+    return (
+        <>
+            <li className="cards">
+                Task: {task} <br/> Student: {student} <br/> <br/>  Check is complete
+                
+                
+                <input type="checkbox" 
+                    checked={isCompleted}
+                    onChange={() => {
+                            handlePut({
+                                task,
+                                student,
+                                id,
+                                isCompleted: !isCompleted
+                            })
+                    }}
+                className="form-check-input"/>
+                <button onClick={()=> {
+                    handleDelete(id);
+                }} className="btn btn-danger" >DELETE</button>
+            </li>
+        </>
+    )
+}
+
+export default TodoItem;
